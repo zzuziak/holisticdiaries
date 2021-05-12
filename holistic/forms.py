93c8745c8cbd6bfb91d8077cde3cc01f0b8a505e
Zuza Zuber <zuza@lewagon.org>
2021-05-12 17:08:32 +0200

    fix setting.py

--- holistic/forms.py
+++ holistic/forms.py
@@ -4,10 +4,10 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'post_summary', 'author', 'body')
+        fields = ('title', 'author', 'post_summary', 'body')
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'post_summary': forms.Textarea(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Write the title'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'post_summary': forms.Textarea(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'})
         }
--- holisticdiaries/settings.py
+++ holisticdiaries/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
