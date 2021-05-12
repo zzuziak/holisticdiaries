3c381b097ec384c70f800fdfcee1e960e518e415
Zuza Zuber <zuza@lewagon.org>
2021-05-12 16:59:33 +0200

    changes of forms

--- holistic/forms.py
+++ holistic/forms.py
@@ -4,10 +4,10 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'post_summary', 'body')
-        widget = {
+        fields = ('title', 'post_summary', 'author', 'body')
+        widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'post_summary': forms.TextArea(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'})
+            'post_summary': forms.Textarea(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'})
         }
--- holistic/views.py
+++ holistic/views.py
@@ -2,6 +2,7 @@ from holistic.models import Post
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 # def home(request):
     # return render(request, 'home.html', {})
 
@@ -15,6 +16,7 @@ class PostView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
     # fields = '__all__'
-    fields = ('title', 'post_summary', 'body')
+    # fields = ('title', 'post_summary', 'body')
