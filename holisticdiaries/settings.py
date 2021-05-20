e9af1536ecae5fa9316fdbac2425c8733eb1b630
Zuza Zuber <zuza@lewagon.org>
2021-05-20 10:04:20 +0200

    remove secret

--- holisticdiaries/settings.py
+++ holisticdiaries/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j9kyahmsmw41ip(zr5bh_(8zxl$q3e%o6*85m$yx-2sy_&%69&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
